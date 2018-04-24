ff7fedc7f8ff0a742330885d3d08b7a117865b68
ErikFrancis <36007823+ErikFrancis@users.noreply.github.com>
2018-04-23 23:51:45 -0700

    Add files via upload

---
+++ Unit 17/src/Ship.java
@@ -0,0 +1,90 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import java.io.File;
+import java.net.URL;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+
+public class Ship extends MovingThing
+{
+	private int speed;
+	private Image image;
+
+	public Ship()
+	{
+		this(10,10,10,10,10);
+	}
+
+	public Ship(int x, int y)
+	{
+		this(x,y,10,10,10);
+	}
+
+	public Ship(int x, int y, int s)
+	{
+		this(x,y,10,10,s);
+	}
+
+	public Ship(int x, int y, int w, int h, int s)
+	{
+		super(x, y, w, h);
+		speed=s;
+		try
+		{
+//			URL url = getClass().getResource("H:\\APCSA\\Workspace\\Unit 17\\src\\ship.jpg");
+			image = ImageIO.read(new File("F:\\Documents\\Eclipse\\Unit 17\\src\\ship.jpg"));
+		}
+		catch(Exception e)
+		{
+			System.out.println("error in constructor");
+		}
+	}
+
+
+	public void setSpeed(int s)
+	{
+	   speed = s;
+	}
+
+	public int getSpeed()
+	{
+	   return speed;
+	}
+
+	public void move(String direction)
+	{
+		if (direction.equals("RIGHT")){
+			setX(getX()+getSpeed());
+		} else if (direction.equals("LEFT")) {
+			setX(getX()-getSpeed());
+		} else if (direction.equals("UP")) {
+			setY(getY()-getSpeed());
+		} else if (direction.equals("DOWN")) {
+			setY(getY()+getSpeed());
+		}
+	}
+
+	public void draw( Graphics window )
+	{
+   	window.drawImage(image,getX(),getY(),getWidth(),getHeight(),null);
+	}
+
+	public void remove() {
+		setX(-10);
+		setY(-10);
+		setWidth(0);
+		setHeight(0);
+		setSpeed(0);
+	}
+	
+	public String toString()
+	{
+		return super.toString() + getSpeed();
+	}
+}
